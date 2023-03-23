24203d2e5e49badb60f010574130cd923b6c0a22
Tom Grant <grntthomasronnie@gmail.com>
2023-03-23 21:51:35 +0000

    Added responsive styling to components

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -20,7 +20,7 @@ return(
             <div className="nav-links">
                     <a className="nav-links" href="https://github.com/TomRgrant" target="_blank"> <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="git hub logo, link to thomas grant's github profile"/> </a>
                     <a className="nav-links" href="https://www.linkedin.com/in/thomas-grant-1063b126a/" target="_blank"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" alt="linkedin logo, link to thomas grant's linkedinpage"/> </a>
-                    <a className="nav-links" href="https://www.youtube.com/" target="_blank"> <img src="https://cdn.worldvectorlogo.com/logos/youtube-3.svg" alt="youtube logo, link to thomas grant's youtube page"/> </a>
+                    {/* <a className="nav-links" href="https://www.youtube.com/" target="_blank"> <img src="https://cdn.worldvectorlogo.com/logos/youtube-3.svg" alt="youtube logo, link to thomas grant's youtube page"/> </a> */}
             </div>
         </section>
     </>
